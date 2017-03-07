03c4c41aaf051c1dcc958f7f8be58cd3a398d70a
sundusk <zhuyue346@gmail.com>
2017-03-07 10:45:10 +0800

    创建项目

---
+++ BlueTooth/BlueTooth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlueTooth
+//
+//  Created by sundusk on 2017/3/7.
+//  Copyright © 2017年 sundusk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlueTooth/BlueTooth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlueTooth
+//
+//  Created by sundusk on 2017/3/7.
+//  Copyright © 2017年 sundusk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
